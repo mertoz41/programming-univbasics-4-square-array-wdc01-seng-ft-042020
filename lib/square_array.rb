f221ba22f0faedb8084df2f78b2e39b90704f76a
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-03-18 17:54:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
   counter = 0 
-  while counter < array[counter] do 
+  while counter < array.length do 
     new_array.push(array[counter]**2)
     counter += 1 
   end
